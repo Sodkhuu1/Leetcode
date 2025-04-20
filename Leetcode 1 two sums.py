3bd95796fae372d91757bd6fbf17193379b68bb6
Sodkhuu1 <bsodkhuu0604@gmail.com>
2025-04-20 21:08:32 +0800

    two sums final push

--- Leetcode 1 two sums.py
+++ Leetcode 1 two sums.py
@@ -1,3 +1,4 @@
+import random
 class Solution(object):
     def twoSum(self, nums, target):
         seen = {}
@@ -7,3 +8,7 @@ class Solution(object):
                 return [seen[diff], i]
             else:
                 seen[nums[i]] = i
+nums = [random.randrange(100) for i in range(100)]
+target = int(input("Enter the target 1-150: "))
+res = Solution().twoSum(nums, target)
+print(res)
